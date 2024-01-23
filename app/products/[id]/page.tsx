0f1866f1c5ddd6eeca1be8f7b05a68349bdc038f
Dimitar <dimitar.mandarliev@gmail.com>
2024-01-23 23:31:55 +0200

    added the type

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,14 +1,57 @@
 import React from "react";
 
 type PageProps = {
-    params: {
-        id: string;
-    }
+  params: {
+    id: string;
+  };
+};
+
+type Product = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+};
+
+// helper function
+const getProduct = async (id: string) => {
+  const res = await fetch(`https://dummyjson.com/products/${id}`, {
+    next: { revalidate: 10 },
+  });
+  const product: Product = await res.json();
+  return product;
+};
+
+async function ProductPage({ params: { id } }: PageProps) {
+  const product = await getProduct(id);
+  return (
+    <div>
+      <h1>I am a single product {product.id}</h1>
+      <h2>{product.description}</h2>
+      <h3>{product.price}</h3>
+    </div>
+  );
 }
 
-function ProductPage({params: {id}}: PageProps) {
+const getProducts = async () => {
+  const res = await fetch(`https://dummyjson.com/products`);
+  const data = await res.json();
+  return data.products;
+};
 
-  return <div>I am a single product {id}</div>;
+export async function generateStaticParams() {
+  const products: Product[] = await getProducts();
+
+  return products.map((product) => ({
+    id: String(product.id),
+  }));
 }
 
 export default ProductPage;
